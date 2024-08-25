a3ce77672dd2598c5b6a6b3b3e6242c222db37e4
valentin <31183973+valentinzamarin@users.noreply.github.com>
2024-08-25 23:17:17 +0400

    Update EnqueueScripts.php
    http to https

--- inc/Frontend/EnqueueScripts.php
+++ inc/Frontend/EnqueueScripts.php
@@ -7,7 +7,7 @@ class EnqueueScripts extends AbstractSingleton{
     function task_manager_enqueue_scripts() {
         $time = time();
         wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css', [], $time);
-        wp_enqueue_style('datepicker', 'http://raw.githack.com/mymth/vanillajs-datepicker/v1.3.2/dist/css/datepicker-bulma.css', [], $time);
+        wp_enqueue_style('datepicker', 'https://raw.githack.com/mymth/vanillajs-datepicker/v1.3.2/dist/css/datepicker-bulma.css', [], $time);
 
 
         wp_enqueue_script('tasks-scripts', TASK_MANAGER_PLUGIN_DIR_URL . '/scripts/tasks.js', ['jquery'], $time, true);
